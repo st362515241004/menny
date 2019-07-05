061ab03465d5f598dac7fa56b25e354164fd8ff8
st362515241004 <52311306+st362515241004@users.noreply.github.com>
2019-07-05 14:08:25 +0700

    Add files via upload
    โปรเเกรมเเรก

---
+++ Lukkrok#2 Ex.1.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+print("--------------------------------------")
+print("M bear shop")
+print("--------------------------------------")
+print("1.civit x 3    : 45    THB")
+print("2.pizza x 2   : 50    THB")
+print("3.dulex x 2   : 150 THB")
+print("--------------------------------------")
+print("Totel           : 245  THB")
+
+
+# In[13]:
+
+
+print(type(10))
+print(type("Hello"))
+print(type(True))
+print(type(10.3))
+print(type(1j))
+
+
+# In[15]:
+
+
+print((10+3.5))
+
+
+# In[ ]:
+
+
+
+
