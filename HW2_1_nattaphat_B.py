3e05ac48cb85af726fc69e894acb2b8644bfd22b
st362515241004 <52311306+st362515241004@users.noreply.github.com>
2019-07-22 20:42:51 +0700

    HW2

---
+++ HW2_1_nattaphat_B.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#นาย ณัฐภัทร บุญรอด 362515241004 EE36241N
+print("--------------Menu--------------")
+print("Welcome to Icecream Shop")
+print("1. Strawberry 80 THB")
+print("2. Vanila 50 THB")
+print("3. Chocolate 40 THB")
+print("4. Lemon 90 THB")
+print("5. Milk 10 THB")
+Icecream=int(input("What do you want?"))
+if Icecream==1 :
+    print("You order : 1 Strawberry 80 THB")
+elif Icecream==2 :
+    print("You order : 2 Vanila 50 THB")
+elif Icecream==3 :
+    print("You order : 3 Chocolate 40 THB")
+elif Icecream==4 :
+    print("You order : 4 Lemon 90 THB")
+elif Icecream==5 :
+    print("You order : 5 Milk 10 THB")
+
+
+# In[ ]:
+
+
+
+
